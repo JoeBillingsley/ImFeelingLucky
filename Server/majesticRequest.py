93812d49d611c28536a95da27510077cdd2ae10a
stats94 <stats1994@live.co.uk>
2015-03-29 02:11:37 +0100

    OMG THIS IS THE WORST THING I'VE EVER DONE

--- Server/majesticRequest.py
+++ Server/majesticRequest.py
@@ -1,8 +1,11 @@
 import urllib3, json, traceback, requests, re
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import tinycss
+
 apiKey = '20DE5AE7C82056A29670B0458D3871DB'
 majurl = 'http://developer.majestic.com/api/json?app_api_key='+str(apiKey)+'&cmd='
 def backlinkRequest(siteRequest):
-	http = urllib3.PoolManager()
 	limit = 100
 	try:
 		url = str(majurl)+'GetBackLinkData&item='+str(siteRequest)+'&Count='+str(limit)+'&datasource=fresh&MaxSourceURLsPerRefDomain=1'
@@ -33,7 +36,6 @@ def getSourceURL(r):
 	return l
 
 def refDomains(siteRequest):
-	http = urllib3.PoolManager()
 	limit = 100
 	try:
 		url = str(majurl)+'GetBackLinkData&item='+str(siteRequest)+'&Count='+str(limit)+'&datasource=fresh&MaxSourceURLsPerRefDomain=1'
@@ -42,7 +44,82 @@ def refDomains(siteRequest):
 	except Exception:
 		print(traceback.format_exc())
 
-request = refDomains('www.bbc.co.uk')
+def imgScrape(site):
+	http = urllib3.PoolManager()
+	filehandle = http.urlopen('GET',site)
+	soup = BeautifulSoup(filehandle.data)
+	imageArr = []
+	for image in soup.find_all('img'):
+		imageArr.append(image["src"])
+	return imageArr
 
-for row in request:
-	print(row)
+def cssScrape(site):
+	"""
+	driver = webdriver.Firefox()
+	driver.get('http://'+site)
+
+	element = driver.find_element_by_id('html')
+	print(element.value_of_css_property('background-color'))
+	"""
+
+	http = urllib3.PoolManager()
+	filehandle = http.urlopen('GET',site)
+	soup = BeautifulSoup(filehandle.data)
+
+	inlineCSS = soup.find_all('style',attrs={'type':'text/css'})
+
+	cssCode = ''
+
+	if inlineCSS != None:
+		for css in inlineCSS:
+			#print(css.text)
+
+			c = css.text.split('\n')
+
+			for x in c:
+				if 'background' in x:
+					#print(x)
+
+					y = x.split(';')
+
+					for z in y:
+						if 'background' in z:
+							a = z.split(':')
+							for b in a:
+								if '#' in b:
+									if cssCode == '':
+										cssCode = b									
+
+			#print(css.text)
+			#parser = tinycss.make_parser('page3')
+			#stylesheet = parser.parse_stylesheet(css.text)
+
+			#print(stylesheet.rules)
+	else:
+		for css in soup.find_all('link',attrs = {'type':'text/css'}):
+			#print (css)
+
+			filehandleCSS = http.urlopen('GET',css["href"])
+			#print(filehandleCSS.data)
+
+			#soupCSS = BeautifulSoup(filehandleCSS.data)
+
+			parser = tinycss.make_parser('page3')
+			stylesheet = parser.parse_stylesheet_bytes(filehandleCSS.data)
+			print(stylesheet.rules)
+
+			for x in stylesheet.rules:
+				if 'background' in x:
+					a = x.split(':')
+					for b in a:
+						if '#' in b:
+							if cssCode == '':
+								cssCode = b
+
+	if cssCode == '':
+		cssCode = '#000'
+
+	return cssCode	
+
+print(cssScrape('http://outlinenone.com/'))
+#print(trustRequest('http://www.reddit.com'))
